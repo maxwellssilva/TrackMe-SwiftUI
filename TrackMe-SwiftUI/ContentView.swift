4ec0a991b27e3d9ab5f66684e8c3879bcaacd717
Maxwell Silva <maxwell.ssilvva@gmail.com>
2025-04-05 21:43:44 -0400

    Initial Commit

---
+++ TrackMe-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TrackMe-SwiftUI
+//
+//  Created by Maxwell Silva on 05/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrackMe-SwiftUI/TrackMe_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackMe_SwiftUIApp.swift
+//  TrackMe-SwiftUI
+//
+//  Created by Maxwell Silva on 05/04/25.
+//
+
+import SwiftUI
+
+@main
+struct TrackMe_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
